e49ce3d67e8804f7e5e3f164f07ff5e4cac7b94c
HamzaMShoaib <hamzamshoaib@gmail.com>
2015-05-10 17:06:00 +1000

    add stuff to item table

--- te_assignment_2/WebContent/schema.sql
+++ te_assignment_2/WebContent/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE cast_db.WishList;
 DROP TABLE cast_db.BiddingPrice;
 DROP TABLE cast_db.Items;
 DROP TABLE cast_db.Users;
@@ -18,7 +19,12 @@ CREATE TABLE cast_db.Items (
 	Name VARCHAR(255) NOT NULL,
 	Owner VARCHAR(255) REFERENCES cast_db.Users(UserName),
 	Description VARCHAR(2000),
-	ReservePrice INT NOT NULL
+	Picture VARCHAR(2000) DEFAULT 'https://abs.twimg.com/sticky/default_profile_images/default_profile_0_400x400.png',
+	ReservePrice INT NOT NULL,
+	StartingPrice INT DEFAULT 1,
+	Duration INT DEFAULT 600,
+	Increments INT DEFAULT 1,
+	Address VARCHAR(255) REFERENCES cast_db.Users(Address)
 );
 
 CREATE TABLE cast_db.BiddingPrice (
@@ -33,7 +39,7 @@ CREATE TABLE cast_db.WishList (
 	UserName VARCHAR(255) REFERENCES cast_db.Users(UserName),
 	Item_ID INT REFERENCES cast_db.Items(Item_ID),
 	PRIMARY KEY (UserName, Item_ID)
-)
+);
 
 INSERT INTO cast_db.Users VALUES ('Hamza', 'password', 'Hamza', 'Shoaib', 'hamza@gmail.com', 'Glenwood', '2015-02-24');
 INSERT INTO cast_db.Users VALUES ('Godlin', 'password', 'Godlin', 'Raja', 'godyraja@gmail.com', 'Glenwood', '2015-02-24');
