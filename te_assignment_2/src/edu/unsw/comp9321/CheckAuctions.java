9b3c1f3a3674011c601f3d0fc4cfcca781716699
Kagra <karn.agrawal@gmail.com>
2015-05-13 23:21:31 +1000

    Fixed error in getReservedPrice. Stopped that damn timer from printing every 5secs

--- te_assignment_2/src/edu/unsw/comp9321/CheckAuctions.java
+++ te_assignment_2/src/edu/unsw/comp9321/CheckAuctions.java
@@ -12,7 +12,7 @@ public class CheckAuctions implements Runnable {
 	public void run() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
-		System.out.println(dateFormat.format(cal.getTime()));
+//		System.out.println(dateFormat.format(cal.getTime()));
 		
 		//For Item_DB whose time is before now
 		BiddingController bc = new BiddingController();
